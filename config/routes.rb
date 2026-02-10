1098e63efa187fb436fa924cdfce9cef63f59119
ryu <kaigo2013.ryuuiti@gmail.com>
2026-02-10 19:07:41 +0900

    ルートをAPIに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :shifts
-  resources :names
-  resources :shift_types
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users
+    resources :shifts
+    resources :names
+    resources :shift_types
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "/api/up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+    get "/up" => "rails/health#show", as: :rails_health_check
+  end
 end
